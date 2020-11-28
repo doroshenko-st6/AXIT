b0f9f33247a6f1263a55956025d9d7764c60dbde
Doroshenko_st6 <lightestwaymusic@gmail.com>
2020-11-28 15:30:36 +0200

    sp 300

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ $('.testimonial-grid').slick({
 $('.pricing-grid').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
-  speed: 100,
+  speed: 300,
 
   prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
   nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
